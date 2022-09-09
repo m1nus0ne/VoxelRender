ab66cc4a15bf6877ae54435ae760ef497c2c6aa3
m1nus0ne <Voldemar7455@Gmail.com>
2022-09-09 19:32:50 +0500

    finally

--- VoxelRender/Form1.cs
+++ VoxelRender/Form1.cs
@@ -9,11 +9,9 @@ public partial class Form1 : Form
 
     public Form1(VoxelRendering render)
     {
-        ;
         _rendering = render;
         KeyDown += (sender, args) => KeysHandler.OnPress(args);
         KeyUp += (sender, args) => KeysHandler.OnRelease(args);
-
         var tmr = new Timer();
         tmr.Interval = 10;
         tmr.Tick += (sender, args) => Invalidate();
@@ -23,14 +21,14 @@ public partial class Form1 : Form
 
     protected override void OnPaint(PaintEventArgs e)
     {
-        Size = new Size(1000, 800);
+        Size = new Size(Config.WindowWindth, Config.WindowHeight);
 
         DoubleBuffered = true;
         var g = e.Graphics;
         _rendering.Update();
         var myBrush = new SolidBrush(Color.White);
         var myFont = new Font("arial", 30);
-        g.DrawImage(_rendering.screenImage.GetBmp(), 0, 0, 1000, 800);
+        g.DrawImage(_rendering.screenImage.GetBmp(), 0, 0, Config.WindowWindth, Config.WindowHeight);
         g.DrawString(_rendering.Player.Pos.ToString(), myFont, myBrush, 20, 20);
         g.DrawString(_rendering.Player.Angles.ToString(), myFont, myBrush, 20, 70);
         g.DrawString(_rendering.elapsedTime, myFont, myBrush, 20, 100, new StringFormat());
